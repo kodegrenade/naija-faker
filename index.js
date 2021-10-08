a29c0cbda9723777f9e50e7a0b5dd805397ef51a
codegrenade <brhamix@gmail.com>
2021-10-08 16:12:57 +0100

    Removed development code block

--- index.js
+++ index.js
@@ -4,8 +4,4 @@
  * License: MIT
  */
 const faker = require('./src/Faker/faker')
-for (let index = 0; index < 5; index++) {
-    // faker.config({ language: "yoruba", gender: "male" });
-    console.log(faker.people(6))
-}
-// module.exports = faker
+module.exports = faker
